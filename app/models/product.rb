a6aedf1081251f00b4f22fdf7cc4a93bd63fe1d4
nakata yuki <nasutaro211@gmail.com>
2018-07-24 15:28:42 +0900

    add validation to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_many :comments
+  validates :name, presence: true
+  validates :about, presence: true
+  validates :need_people, presence: true
+  validates :goal, presence: true
+
 end
